b81ce27e8ce1c8ef3492b8960f237494511f8e0c
Mahdi Chowdhury <142717756+mahdi-chy@users.noreply.github.com>
2023-08-20 15:34:57 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include"iostream"
+#include"fstream"
+#include"string"
 
+using namespace std;
+int main(int argc, char *argv[])
+{
+		fstream mFile;
+		mFile.open("test.txt",ios::in);
+		string line;
+		int qno;
+		getline(mFile,line);
+		qno=stoi(line);
+		
+		while(getline(mFile,line)){
+			cout<<line<<endl;
+		}
+		mFile.close();
+}
