f0fe2cc002658bd256779566203fc43aa8edba0b
Mahdi Chowdhury <142717756+mahdi-chy@users.noreply.github.com>
2023-08-21 20:59:22 +0600

    Funtional_file main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include"iostream"
 #include"fstream"
 #include"string"
-
 using namespace std;
+struct qz{
+	string q;
+	string o1;
+	string o2;
+	string o3;
+	int a;
+	};
+int storeQuiz(){
+	
+	}
+
+
 int main(int argc, char *argv[])
 {
+		int input=0;
 		fstream mFile;
 		mFile.open("test.txt",ios::in);
 		string line;
@@ -12,8 +24,46 @@ int main(int argc, char *argv[])
 		getline(mFile,line);
 		qno=stoi(line);
 		
-		while(getline(mFile,line)){
-			cout<<line<<endl;
+		qz quiz[qno];
+		
+		
+		for(int i=0;i<qno;i++){
+				getline(mFile,quiz[i].q);
+				getline(mFile,quiz[i].o1);
+				getline(mFile,quiz[i].o2);
+				getline(mFile,quiz[i].o3);
+				getline(mFile,line);
+				quiz[i].a=stoi(line);
 		}
+		for (int i=0;i<qno;i++){
+			cout<<quiz[i].q<<endl;
+			cout<<quiz[i].o1<<endl;
+			cout<<quiz[i].o2<<endl;
+			cout<<quiz[i].o3<<endl;
+			
+			cout<<"Type one of these(1,2,3): ";
+			cin>>input;
+			switch (quiz[i].a){
+				case 1:
+					if(quiz[i].a==input){
+						cout<<"Correct!"<<endl;
+						}else{cout<<"Wrong Answer!"<<endl;}
+						break;
+				case 2:
+					if(quiz[i].a==input){
+						cout<<"Correct!"<<endl;
+						}else{cout<<"Wrong Answer!"<<endl;}
+						break;
+				case 3:
+					if(quiz[i].a==input){
+						cout<<"Correct!"<<endl;
+						}else{cout<<"Wrong Answer!"<<endl;}
+						break;
+				default:
+					cout<<"Wrong Answer!"<<endl;
+					break;
+		}
+		cout<<endl;
+						}
 		mFile.close();
 }
