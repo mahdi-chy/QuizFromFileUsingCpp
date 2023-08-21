4b75143b7ae49605a70a0c7dfc16740f42a96200
Mahdi Chowdhury <142717756+mahdi-chy@users.noreply.github.com>
2023-08-22 00:18:22 +0600

    little problem in line 24 main.cpp

--- main.cpp
+++ main.cpp
@@ -13,55 +13,55 @@ struct qz{
 
 int main(int argc, char *argv[])
 {
-		int input=0;
-		fstream mFile;
-		mFile.open("test.txt",ios::in);
-		string line;
-		int qno;
+	int input=0;
+	fstream mFile;
+	mFile.open("test.txt",ios::in);
+	string line;
+	int qno;
+	getline(mFile,line);
+	qno=stoi(line);
+		
+	qz quiz[qno];
+//this here is a bit of a problem for now, since arrays only takes const value
+		
+		
+	for(int i=0;i<qno;i++){
+		getline(mFile,quiz[i].q);
+		getline(mFile,quiz[i].o1);
+		getline(mFile,quiz[i].o2);
+		getline(mFile,quiz[i].o3);
 		getline(mFile,line);
-		qno=stoi(line);
-		
-		qz quiz[qno];
-	//this here is a bit of a problem for now, since arrays only takes const value
-		
-		
-		for(int i=0;i<qno;i++){
-				getline(mFile,quiz[i].q);
-				getline(mFile,quiz[i].o1);
-				getline(mFile,quiz[i].o2);
-				getline(mFile,quiz[i].o3);
-				getline(mFile,line);
-				quiz[i].a=stoi(line);
+		quiz[i].a=stoi(line);
 		}
-		for (int i=0;i<qno;i++){
-			cout<<quiz[i].q<<endl;
-			cout<<quiz[i].o1<<endl;
-			cout<<quiz[i].o2<<endl;
-			cout<<quiz[i].o3<<endl;
+	for (int i=0;i<qno;i++){
+		cout<<quiz[i].q<<endl;
+		cout<<quiz[i].o1<<endl;
+		cout<<quiz[i].o2<<endl;
+		cout<<quiz[i].o3<<endl;
 			
-			cout<<"Type one of these(1,2,3): ";
-			cin>>input;
-			switch (quiz[i].a){
-				case 1:
-					if(quiz[i].a==input){
-						cout<<"Correct!"<<endl;
-						}else{cout<<"Wrong Answer!"<<endl;}
-						break;
-				case 2:
-					if(quiz[i].a==input){
-						cout<<"Correct!"<<endl;
-						}else{cout<<"Wrong Answer!"<<endl;}
-						break;
-				case 3:
-					if(quiz[i].a==input){
-						cout<<"Correct!"<<endl;
-						}else{cout<<"Wrong Answer!"<<endl;}
-						break;
-				default:
-					cout<<"Wrong Answer!"<<endl;
+		cout<<"Type one of these(1,2,3): ";
+		cin>>input;
+		switch (quiz[i].a){
+		 case 1:
+			if(quiz[i].a==input){
+				cout<<"Correct!"<<endl;
+					}else{cout<<"Wrong Answer!"<<endl;}
 					break;
+		case 2:
+			if(quiz[i].a==input){
+				cout<<"Correct!"<<endl;
+					}else{cout<<"Wrong Answer!"<<endl;}
+					break;
+		case 3:
+			if(quiz[i].a==input){
+				cout<<"Correct!"<<endl;
+					}else{cout<<"Wrong Answer!"<<endl;}
+					break;
+		default:
+			cout<<"Wrong Answer!"<<endl;
+				break;
 		}
-		cout<<endl;
-						}
-		mFile.close();
+	cout<<endl;
+	}
+	mFile.close();
 }
