3e3970fd777766df2e48ae0622a98098f285dda7
Mahdi Chowdhury <142717756+mahdi-chy@users.noreply.github.com>
2023-10-18 11:17:37 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,88 +1,73 @@
-#include"iostream"
-#include"fstream"
-#include<string>
-#include<vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 using namespace std;
-struct qz{
-	string q;
-	string o1;
-	string o2;
-	string o3;
-	int a=0;
+
+struct qz {
+    string q;
+    string o1;
+    string o2;
+    string o3;
+    int a = 0;
 };
 
-int main(int argc, char* argv[]){
-	int input = 0, rAns = 0, wAns = 0;
-	fstream mFile;//creats a mFile variable to access "test.txt" file
-	mFile.open("test.txt", ios::in);
-	string line;
-	int quizNo;
-	getline(mFile, line);//gets the first line from "test.txt file"
-	quizNo = stoi(line);//converts the first line of "test.txt" file to int and stores in qno variable
-     
-      //qz quiz[quizNo];this is what i want to use.
-	qz quiz[100];//making quiz variable of qz structure that I made above
-//also, I coudn't find a way to declare the quiz structure array so it's gonna be pre-declared for the time being untill I get more knowledge about this
-	
-	for (int i = 0; i < quizNo; i++){
-		getline(mFile, line);
-		quiz[i].q = line;
-		getline(mFile, line);
-		quiz[i].o1 = line;
-		getline(mFile, line);
-		quiz[i].o2 = line;
-		getline(mFile, line);
-		quiz[i].o3 = line;
-		getline(mFile, line);
-		quiz[i].a = stoi(line);
-	}
+int main(int argc, char* argv[]) {
+    int input = 0, rAns = 0, wAns = 0;
+    fstream mFile;
 
-	for (int i = 0; i < quizNo; i++){
-		cout << quiz[i].q << endl;
-		cout << quiz[i].o1 << endl;
-		cout << quiz[i].o2 << endl;
-		cout << quiz[i].o3 << endl;
+    // Opening the input file and checking if it's opened successfully
+    mFile.open("test.txt", ios::in);
+    if (!mFile.is_open()) {
+        cout << "Error opening the file." << endl;
+        return 1;
+    }
 
-		cout << "Type one of these(1,2,3): ";
-		cin >> input;
-		switch (quiz[i].a){
-		case 1:
-			if(quiz[i].a == input){
-				cout << "Correct!" << endl;
-				rAns++;
-			}else{
-				cout << "Wrong Answer!" << endl;
-				wAns++;
-			}
-			break;
-		case 2:
-			if (quiz[i].a == input){
-				cout << "Correct!" << endl;
-				rAns++;
-			}else{
-				cout << "Wrong Answer!" << endl;
-				wAns++;
-			}
+    string line;
+    int quizNo;
+    getline(mFile, line);
+    quizNo = stoi(line);
 
-			break;
-		case 3:
-			if (quiz[i].a == input){
-				cout << "Correct!" << endl;
-				rAns++;
-			}else{
-				cout << "Wrong Answer!" << endl;
-				wAns++;
-			}
-			break;
-		default:
-			cout << "Wrong Answer!" << endl;
-			wAns++;
-			break;
-		}
+    // we are using vector to store quiz questions
+    vector<qz> quiz(quizNo);
 
-		cout << endl;
-	}
+    for (int i = 0; i < quizNo; i++) {
+        getline(mFile, line);
+        quiz[i].q = line;
+        getline(mFile, line);
+        quiz[i].o1 = line;
+        getline(mFile, line);
+        quiz[i].o2 = line;
+        getline(mFile, line);
+        quiz[i].o3 = line;
+        getline(mFile, line);
+        quiz[i].a = stoi(line);
+    }
 
-	mFile.close();
-	cout << "Your Scoreboard:\nRight Ans:" << rAns << "  Wrong Ans:" << wAns << endl;
+    //showing the quiz to user and taking answers while recording the scoreboard	
+    for (int i = 0; i < quizNo; i++) {
+        cout << quiz[i].q << endl;
+        cout << quiz[i].o1 << endl;
+        cout << quiz[i].o2 << endl;
+        cout << quiz[i].o3 << endl;
+
+        cout << "Type one of these (1, 2, 3): ";
+        cin >> input;
+
+        if (quiz[i].a == input) {
+            cout << "Correct!" << endl;
+            rAns++;
+        } else {
+            cout << "Wrong Answer!" << endl;
+            wAns++;
+        }
+
+        cout << endl;
+    }
+
+    mFile.close();
+    cout << "Your Scoreboard:\nRight Ans: " << rAns << "  Wrong Ans: " << wAns << endl;
+
+    return 0;
 }
